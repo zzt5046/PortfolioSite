b4f6edbe056816c21413f2f2318637a5f3de2486
Zach <zzt5046@psu.edu>
2019-01-21 00:10:16 -0500

    Update registerBackend.php

--- php/registerBackend.php
+++ php/registerBackend.php
@@ -1,5 +1,20 @@
-<html>
-<body>
-not yet implemented
-</body>
-</html>
+<?php
+
+	include_once '../cfg/conn.php';
+
+	$user = $_POST["user"];
+	$pass = $_POST["pass"];
+	$confirm = $_POST["confirm"];
+	$admin = 0;
+
+	if($pass == $confirm){
+		$sql = "INSERT INTO phpaccess (username, password, admin) VALUES ($user, $pass, $admin)"
+		$input = mysqli_query($conn, $sql);
+
+		header("Location: http://www.yourwebsite.com/php/login.php"); /* Redirect browser */
+		exit();
+	}
+	
+	echo "Passwords do not match.";
+	exit();
+?>
