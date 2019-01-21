133284840e506c1e347e1f989ad5d58aa9c22070
Zach <zzt5046@psu.edu>
2019-01-21 00:52:19 -0500

    Update registerBackend.php

--- php/registerBackend.php
+++ php/registerBackend.php
@@ -8,7 +8,7 @@
 	$admin = 0;
 
 	if($pass == $confirm){
-		$sql = "INSERT INTO phpaccess VALUES ('$user', '$pass', '$admin');";
+		$sql = "INSERT INTO phpaccess VALUES ('$user', '$pass', '$admin', null);";
 		mysqli_query($conn, $sql);
 
 		header("Location: login.html?signup=success"); /* Redirect browser */
