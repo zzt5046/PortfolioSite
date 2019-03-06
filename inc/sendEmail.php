1a6b167e0e6b1a716e383cb8c6a3b0acdabbe40f
Zach <zzt5046@psu.edu>
2019-03-06 12:15:16 -0500

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -50,7 +50,7 @@ if($_POST) {
       //$mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Something went wrong. Please try again. Does this change"; }
 		
 	} # end if - no validation error
 
