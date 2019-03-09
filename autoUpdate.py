d3eab6f77f2b8f7efec1cf79e31f348129003472
Zach <zzt5046@psu.edu>
2019-03-08 19:16:18 -0500

    Update autoUpdate.py

--- autoUpdate.py
+++ autoUpdate.py
@@ -3,7 +3,7 @@ import os
 import time
 import subprocess
 
-status = subprocess.check_output(['git', 'status'], shell=True).decode('utf-8')
+status = subprocess.check_output(['sudo', 'git', 'status'], shell=True).decode('utf-8')
 update = "./update.py"
 
 print(str(status))
